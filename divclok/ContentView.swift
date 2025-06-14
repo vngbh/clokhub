8d4f48b20c09d1c646d14ee95c6d39119e8b6012
vngbh <154517591+vngbh@users.noreply.github.com>
2025-06-15 03:22:20 +0900

    Initial Commit

---
+++ divclok/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  divclok
+//
+//  Created by baohoang on 2025/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ divclok/divclokApp.swift
@@ -0,0 +1,17 @@
+//
+//  divclokApp.swift
+//  divclok
+//
+//  Created by baohoang on 2025/06/15.
+//
+
+import SwiftUI
+
+@main
+struct divclokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
